ff60d1c8df8beded83bb2ba9c9c3a5277ded2901
Alex Kass <alexrkass@gmail.com>
2016-01-18 21:38:49 -0800

    install jekyll

---
+++ scripts/default.js
@@ -0,0 +1,7 @@
+var AFRAME = require('aframe');
+
+var NoClickLookControls = require('aframe-no-click-look-controls');
+AFRAME.registerComponent('no-click-look-controls', NoClickLookControls);
+
+var TextComponent = require ('aframe-text-component');
+AFRAME.registerComponent('text', TextComponent);
